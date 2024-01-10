fbf0aef0bb8399dc69765ca5e3681179aa87a4b0
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2024-01-10 16:17:55 -0700

    Started a new module for large-scale distributed tasks on a cluster + more two agent simulations

---
+++ spamdfba/distribute.py
@@ -0,0 +1,25 @@
+from toolkit import Simulation
+import ray
+from typing import Iterable
+
+class TaskServer:
+    def __init__(self,
+                 tasks:Iterable[Simulation],
+                 num_workers:int,
+                 job_info:dict,
+                 ):
+        pass
+
+    def validate(self):
+        pass
+
+class EnvGenerator: 
+    """This class is designed for building large amounts of environments for large-scale experiments.
+    The base environment object should be provided to the constructor."""
+    pass
+
+
+
+
+   
+    
